ca98dc22a87eea52ce23648de991a0c59c14c055
Pham Hanh <48047723+Joypham@users.noreply.github.com>
2019-03-29 19:04:35 +0700

    Update Fix format albums_image_url.sql

--- Fix format albums_image_url.sql
+++ Fix format albums_image_url.sql
@@ -8,4 +8,23 @@ set
 WHERE
 	square_image_url LIKE '%http%'
 	and valid > 0
--- 
+-- Fix format tracks.image_url: 3131
+								   
+UPDATE tracks
+Join(
+SELECT
+	id,
+	title,
+	Artist,
+	ImageURL,
+	REPLACE ( ImageURL, 'https://uprof.vibbidi-vid.com/', '' ) as a
+FROM
+	tracks 
+WHERE
+	ImageURL LIKE '%https://uprof.vibbidi-vid.com/https%'
+	-- and 
+	-- id = 'D4F15E49818849718EB5126B04D53E6C'
+	) as t1
+	on t1.id = tracks.id
+	set
+	tracks.ImageURL = t1.a
